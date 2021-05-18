9796f3b3529b86590de323f6c2d190603c703f9f
Daniel Luca <daniel.luca.bp@nttdata.ro>
2021-05-18 19:41:47 +0300

    Studenti App (grupa1) first release

---
+++ StudentiApp1/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StudentiApp1
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExec)
+        {
+            _execute = execute;
+            _canExecute = canExec;
+        }
+
+        public DelegateCommand(Action<object> execute) : this(execute, null)
+        {  }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter) => _execute?.Invoke(parameter);
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ StudentiApp1/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StudentiApp1
+{
+    public class MainViewModel
+    {
+        private StudentiEntities context;
+
+        public ObservableCollection<Studenti> StudentList { get; set; }
+        public ObservableCollection<Materii> MateriiList { get; set; }
+
+        public ICommand SaveCommand { get; set; }
+
+        public MainViewModel()
+        {
+            context = new StudentiEntities();
+
+            context.Studenti.Load();
+            StudentList = context.Studenti.Local;
+
+            context.Materii.Load();
+            MateriiList = context.Materii.Local;
+
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+        }
+
+        private void OnSaveCommand(object obj)
+        {
+            context.SaveChanges();
+        }
+    }
+}
